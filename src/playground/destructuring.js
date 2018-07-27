6e0ce138e6c1e062077a04eb8d4bbbf630c630f1
Eugene <evgenii.is.the.best.geek@gmail.com>
2018-07-27 13:46:48 +0300

    ES6 Array Destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,35 +1,50 @@
-console.log('destructuring.js');
+//
+// Object destructuring
+//
+// console.log('destructuring.js');
+//
+// const person = {
+//     name: 'Eugene',
+//     age: 23,
+//     location: {
+//         city: 'Zaporizhya',
+//         temp: 28
+//     }
+// };
+//
+// // const name = person.name;
+// // const age = person.age;
+//
+// const {name: firstName = 'Anonymous', age} = person;
+//
+// console.log(`${firstName} is ${age}.`);
+//
+// const {city, temp: temperature} = person.location;
+// if (temperature && city)
+// {
+//     console.log(`It's ${temperature} in ${city}.`);
+// }
+//
+// const book = {
+//     name: 'Ego is an Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
+//
+// const {name: publisherName = 'Self-Published'} = book.publisher;
+//
+// console.log(publisherName);
 
-const person = {
-    name: 'Eugene',
-    age: 23,
-    location: {
-        city: 'Zaporizhya',
-        temp: 28
-    }
-};
+//
+// Array destructuring
+//
 
-// const name = person.name;
-// const age = person.age;
+// const address = ['1299 S Jupiter Street', 'Philadelphia', 'Pennsylvania', '19147'];
+// const [, city, state = 'New York'] = address;
+// console.log(`You're in ${city} ${state}.`);
 
-const {name: firstName = 'Anonymous', age} = person;
-
-console.log(`${firstName} is ${age}.`);
-
-const {city, temp: temperature} = person.location;
-if (temperature && city)
-{
-    console.log(`It's ${temperature} in ${city}.`);
-}
-
-const book = {
-    name: 'Ego is an Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
-};
-
-const {name: publisherName = 'Self-Published'} = book.publisher;
-
-console.log(publisherName);
+const item = ['Coffee (hot)', '$2.00', "$2.50", "$2.75"];
+const [itemName,,mediumPrice] = item;
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
