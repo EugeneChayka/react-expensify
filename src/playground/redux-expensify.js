06a64035d0a769b073cde8e80dd5ffe9069b8a05
Eugene <evgenii.is.the.best.geek@gmail.com>
2018-07-28 12:54:46 +0300

    redux spreading objects

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -24,30 +24,54 @@ const addExpense = (
 });
 
 // Remove expense
-const removeExpense = ({ id } = {}) => ({
+const removeExpense = ({id} = {}) => ({
     type: 'REMOVE_EXPENSE',
     id
+});
+
+// Edit Expense
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
 });
 
 // Expenses Reducer
 
 const expensesReducerDefaultState = [];
 
-const expensesReducer = (state = expensesReducerDefaultState, action) =>
-{
-    switch (action.type)
-    {
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch (action.type) {
         case 'ADD_EXPENSE':
             return [
                 ...state,
                 action.expense
             ];
         case 'REMOVE_EXPENSE':
-            return state.filter(({ id }) => id !== action.id);
+            return state.filter(({id}) => id !== action.id);
+        case 'EDIT_EXPENSE':
+            return state.map((expanse) => {
+                if (expanse.id === action.id) {
+                    return {
+                        ...expanse,
+                        ...action.updates
+                    };
+                }
+                else {
+                    return expanse;
+                }
+            });
         default:
             return state;
     }
 };
+
+// set Text Filter
+
+const setTextFilter = ( text = '' ) => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
 
 // Filters Reducer
 
@@ -58,10 +82,13 @@ const filtersReducersDefaultState = {
     endDate: undefined
 };
 
-const filtersReducer = (state = filtersReducersDefaultState, action) =>
-{
-    switch (action.type)
-    {
+const filtersReducer = (state = filtersReducersDefaultState, action) => {
+    switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            };
         default:
             return state;
     }
@@ -76,8 +103,7 @@ const store = createStore(
     })
 );
 
-store.subscribe(() =>
-{
+store.subscribe(() => {
     console.log(store.getState());
 });
 
@@ -85,8 +111,10 @@ const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 100})
 const expenseTwo = store.dispatch(addExpense({description: 'Coffee', amount: 300}));
 
 store.dispatch(removeExpense({id: expenseOne.expense.id}));
+store.dispatch(editExpense(expenseTwo.expense.id, {amount: 500}));
 
-console.log(expenseOne);
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter(''));
 
 const demoState = {
     expenses: [{
@@ -103,3 +131,15 @@ const demoState = {
         endDate: undefined
     }
 };
+
+// const user = {
+//     name: 'JEn',
+//     age: 24
+// };
+
+// console.log({
+//     age: 39,
+//     ...user,
+//     location: 'Kiev',
+//     age: 27
+// })
