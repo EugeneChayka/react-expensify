0e653adc3698133741295bc4a3a4f76fd68d7bd2
Eugene <evgenii.is.the.best.geek@gmail.com>
2018-07-29 12:33:11 +0300

    React with Redux - Rendering Individual Expense

--- src/app.js
+++ src/app.js
@@ -12,12 +12,12 @@ import './styles/styles.scss';
 const store = configureStore();
 
 //add Expense - water bill
-store.dispatch(addExpense({description: 'Water Bill'}));
+store.dispatch(addExpense({description: 'Water Bill', amount: 4500}));
 store.dispatch(addExpense({description: 'Gas Bill'}));
 store.dispatch(setTextFilter('wat'));
 
 setTimeout(()=>{
-    store.dispatch(setTextFilter('rent'));
+    store.dispatch(setTextFilter('bill'));
 }, 3000);
 
 const state = store.getState();
