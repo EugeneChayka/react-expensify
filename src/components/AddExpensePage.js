29447794705b24d5974947ad98f28c0b8a43960d
Eugene <evgenii.is.the.best.geek@gmail.com>
2018-08-25 22:28:19 +0300

    React with Redux - Wiring up Add Expense Page

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,11 +1,18 @@
 import React from "react";
-import ExpenseForm from './ExpenseForm'
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses'
 
-const AddDashboardPage = () => (
+const AddDashboardPage = (props) => (
     <div>
         <h1>Add Expense</h1>
-        <ExpenseForm/>
+        <ExpenseForm
+            onSubmit={(expense) => {
+                props.dispatch(addExpense(expense));
+                props.history.push('/')
+            }}
+        />
     </div>
 );
 
-export default AddDashboardPage;
+export default connect()(AddDashboardPage);
